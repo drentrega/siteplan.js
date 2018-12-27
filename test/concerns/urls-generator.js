94a8fdbcac11440bf1b1e80fc2bb5f464f3ea2ea
Filipe Forattini <filipeforattini1@gmail.com>
2018-12-27 16:57:07 -0200

    test(test/concerns/urls-generator): right property now for url path

--- test/concerns/urls-generator.js
+++ test/concerns/urls-generator.js
@@ -2,7 +2,7 @@ module.exports = (n = 10) => {
   let urls = [];
   for (let i = 0; i < n; i++) {
     urls.push({
-      link: `/post/${Date.now}`,
+      path: `/post/${Date.now}`,
       priority: parseInt(Math.random()*10)/10,
       updateFrequency: 'daily',
       modifiedAt: new Date(new Date().getTime() + parseInt(Math.abs(Math.random()*1000)**2)),
